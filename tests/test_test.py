b91a0b4dc692663dc4197364984e537fa075dff4
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2024-10-28 18:44:52 +0000

    Linting good

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,9 +1,8 @@
-import pytest
+""" Tests for tests"""
 
 def test_test():
+    """Does nothing"""
     assert True
 
-
-def test_noting():
-    pass
-
+def test_nothing():
+    """ Also does nothing"""
