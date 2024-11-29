e1a49ef23f8d779a978d058b55e4c4a1e376be3e
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2024-11-29 12:56:23 +0000

    Move Instrument from _base

--- pyspinw/_base.py
+++ pyspinw/_base.py
@@ -76,10 +76,3 @@ class Anisotropy:
         else:
             return self._anisotropy_matrix
 
-
-class Instrument:
-    """ Defines an instrument
-
-    This class is responsible for instrument specific details, including resolution, energy and q binning, etc.
-    """
-
---
+++ pyspinw/instrument.py
@@ -0,0 +1,7 @@
+
+class Instrument:
+    """ Defines an instrument
+
+    This class is responsible for instrument specific details, including resolution, energy and q binning, etc.
+    """
+
