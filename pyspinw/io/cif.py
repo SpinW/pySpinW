a8e9347f7255e0a42ded5957afaaed0c2919c9a0
lucas-wilkins <lucas.wilkins.email@gmail.com>
2025-01-20 13:12:24 +0000

    mCif loading, initial stuff

--- pyspinw/io/cif.py
+++ pyspinw/io/cif.py
@@ -1,11 +1,57 @@
-from CifFile import ReadCif, CifFile
+from typing import Callable
+from dataclasses import dataclass
 
-def load_mcif(filename: str):
+import numpy as np
+from CifFile import ReadCif, CifFile
+import spglib
+
+@dataclass
+class CellData:
+    a: float
+    b: float
+    c: float
+    alpha: float
+    beta: float
+    gamma: float
+
+
+def extract_cell_data(data: CifFile) -> CellData:
+    a = data["_cell_length_a"]
+    b = data["_cell_length_b"]
+    c = data["_cell_length_c"]
+    alpha = data["_cell_angle_alpha"]
+    beta = data["_cell_angle_beta"]
+    gamma = data["_cell_angle_gamma"]
+
+    return CellData(a, b, c, alpha, beta, gamma)
+
+def load_mcif(filename: str, block_name: str | None = None):
+    """ Create a unit cell based on the data in an mcif file
+
+    :param filename: string with filename to be opened
+    :param block_name: optional string, name of dataset within file, if not present, this method will give the first one
+    """
+
     data: CifFile = ReadCif(filename)
+
+    # Get the block
+    if block_name is None:
+        block = data.first_block()
+    else:
+        block = data[block_name]
+
+    cell_data = extract_cell_data(block)
+
+    print(cell_data)
     return data
 
+def load_cif(filename: str):
+    """ Create a unit cell based on the data in a cif file """
+
+
 if __name__ == "__main__":
-    # data = load_mcif("../../example_structures/1.2_CuSe2O5.mcif")
-    data = load_mcif("../../example_structures/1.2_CuSe2O5-extended.mcif")
+    data = load_mcif("../../example_structures/1.2_CuSe2O5.mcif")
+    # data = load_mcif("../../example_structures/1.2_CuSe2O5-extended.mcif")
     # data = load_mcif("../../example_structures/1100231.cif")
 
+    # print(data)
