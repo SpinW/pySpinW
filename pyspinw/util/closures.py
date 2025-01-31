791c4178da97036530219e820a32dd9702ac72b8
lucas-wilkins <lucas.wilkins.email@gmail.com>
2025-01-31 17:39:28 +0000

    Removed stuff that went into a class

--- pyspinw/util/closures.py
+++ pyspinw/util/closures.py
@@ -1,27 +1,5 @@
 import numpy as np
-
-Generator = tuple[np.ndarray, np.ndarray, float]
-
-def compose_generators(a: Generator, b: Generator) -> Generator:
-    """ Calculate the composition x -> a(b(x) """
-
-    #
-    # for the vector part
-    # a(x) = Ma x + Va
-    # b(x) = Mb x + Vb
-    #
-    # a(b(x)) = Ma(Mb + Vb) + Va
-    #         = (Ma Mb) + (Ma Vb + Va)
-    #
-
-    return a[0] @ b[0], a[0] @ b[1] + a[1], a[2] * b[2]
-
-
-def order_generators(list[Generator]) -> list[Generator]:
-    """ Put generators in order """
-
-    # Convert to big list, 9 + 3 + 1 = 13 across
-    
+from pyspinw.util.group_generators import Generator
 
 def closure(generators: list[Generator]) -> list[Generator]:
     """ Closure of magnetic space groups"""
