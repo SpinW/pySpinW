50f5176b4f5f1522774c73b8d40bbd37c17ab4e1
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2025-02-20 16:30:20 +0000

    Parsing works

--- pyspinw/util/crysfml_load.py
+++ pyspinw/util/crysfml_load.py
@@ -65,6 +65,7 @@ with resources.open_text("pyspinw.data", "magnetic_data.txt") as file:
         hexagonal_point_operations[op.number] = op
 
     for group_id in range(1651):
+        print(group_id)
 
         # First line, names and numbers
         line = file.readline().strip()
@@ -87,7 +88,6 @@ with resources.open_text("pyspinw.data", "magnetic_data.txt") as file:
 
         # Second line, the type
         line = file.readline().strip()
-
         group_type = int(line)
 
         # Now we have group type dependent parsing, type 4 is exceptional
@@ -95,6 +95,7 @@ with resources.open_text("pyspinw.data", "magnetic_data.txt") as file:
 
             # Type 4 parsing
             line = file.readline().strip()
+            parts = [s for s in line.split(" ") if s != ""]
 
             bnsog_point_op = np.array([int(x) for x in parts[:9]]).reshape(3, 3)
             bnsog_origin_num = np.array([int(x) for x in parts[9:12]])
@@ -109,18 +110,27 @@ with resources.open_text("pyspinw.data", "magnetic_data.txt") as file:
         n_operations = int(line)
 
         # Operations 2, operations details, multiple operations in one line
-        line = file.readline().strip()
-        parts = [s for s in line.split(" ") if s != ""]
+
 
         bns_point_op = []
         bns_translation_num = []
         bns_translation_denom = []
         bns_time_inversion = []
-        for i in range(n_operations):
-            bns_point_op.append(int(parts[6*i])) # 6n + 0
-            bns_translation_num.append([int(parts[6*i + j + 1]) for j in range(3)]) # 6n + 1,2,3
-            bns_translation_denom.append(int(parts[6*i+4])) # 6n + 4
-            bns_time_inversion.append(int(parts[6*i+5])) # 6n + 5
+
+        print(bns_label, len(parts), n_operations)
+
+        operation_lines = n_operations // 4
+        operation_sizes = [4]*operation_lines + [n_operations % 4]
+        operation_sizes = [s for s in operation_sizes if s > 0]
+
+        for line_length in operation_sizes:
+            line = file.readline().strip()
+            parts = [s for s in line.split(" ") if s != ""]
+            for i in range(line_length):
+                bns_point_op.append(int(parts[6*i])) # 6n + 0
+                bns_translation_num.append([int(parts[6*i + j + 1]) for j in range(3)]) # 6n + 1,2,3
+                bns_translation_denom.append(int(parts[6*i+4])) # 6n + 4
+                bns_time_inversion.append(int(parts[6*i+5])) # 6n + 5
 
         # Lattice Vectors 1, number of lattice vectors
         line = file.readline().strip()
@@ -162,14 +172,85 @@ with resources.open_text("pyspinw.data", "magnetic_data.txt") as file:
                 line = file.readline().strip()
                 parts = [s for s in line.split(" ") if s != ""]
 
-                positions_num.append([int(parts[10 * j + k]) for k in range(3)]) # 10n + 0,1,2
-                positions_denom.append(int(parts[10 * j + 3])) # 10n + 3
-                positions_xyz.append([int(parts[10 * j + k + 4]) for k in range(3)]) # 10n + 4,5,6
-                positions_mag.append([int(parts[10 * j + k + 7]) for k in range(3)]) # 10n + 7,8,9
+                positions_num.append([int(parts[k]) for k in range(3)]) # 10n + 0,1,2
+                positions_denom.append(int(parts[3])) # 10n + 3
+                positions_xyz.append([int(parts[k + 4]) for k in range(3)]) # 10n + 4,5,6
+                positions_mag.append([int(parts[k + 7]) for k in range(3)]) # 10n + 7,8,9
 
             wyckoff_sites.append((positions_num, positions_denom, positions_xyz, positions_mag))
 
         # Last part, more type 4 dependent stuff, OG representations
 
         if group_type == 4:
-            pass
+            # OG line 1, number of sites
+            line = file.readline().strip()
+            n_og_operations = int(line)
+
+            operation_lines = n_og_operations // 4
+            operation_sizes = [4] * operation_lines + [n_og_operations % 4]
+            operation_sizes = [s for s in operation_sizes if s > 0]
+
+            # OG operators line
+
+            og_point_op = []
+            og_translation_num = []
+            og_translation_denom = []
+            og_time_inversion = []
+
+            for line_length in operation_sizes:
+                line = file.readline().strip()
+                parts = [s for s in line.split(" ") if s != ""]
+
+                for i in range(line_length):
+
+                    og_point_op.append(int(parts[6 * i]))  # 6n + 0
+                    og_translation_num.append([int(parts[6 * i + j + 1]) for j in range(3)])  # 6n + 1,2,3
+                    og_translation_denom.append(int(parts[6 * i + 4]))  # 6n + 4
+                    og_time_inversion.append(int(parts[6 * i + 5]))  # 6n + 5
+
+            # OG Lattice Vectors 1, number of lattice vectors
+            line = file.readline().strip()
+            n_og_lattice_vectors = int(line)
+
+            # OG Lattice Vectors, actual lattice vectors
+            line = file.readline().strip()
+            parts = [s for s in line.split(" ") if s != ""]
+
+            og_lattice_vectors_num = []
+            og_lattice_vectors_denom = []
+
+            for i in range(n_og_lattice_vectors):
+                og_lattice_vectors_num.append([int(parts[4 * i + j]) for j in range(3)])
+                og_lattice_vectors_denom.append(int(parts[4 * i + 3]))
+
+            # Wyckoff sites 1, number of sites
+            line = file.readline().strip()
+            n_og_wyckoff = int(line)
+
+            # Wyckoff loop
+            og_wyckoff_sites = []
+            for i in range(n_og_wyckoff):
+
+                # Wyckoff outer loop
+                line = file.readline().strip()
+                parts = split_stringy_line(line)
+
+                n_positions = int(parts[0])
+                multiplicity = int(parts[1])
+                label = parts[2].strip()
+
+                # Wyckoff inner loop
+                positions_num = []
+                positions_denom = []
+                positions_xyz = []
+                positions_mag = []
+                for j in range(n_positions):
+                    line = file.readline().strip()
+                    parts = [s for s in line.split(" ") if s != ""]
+
+                    positions_num.append([int(parts[k]) for k in range(3)])  # 10n + 0,1,2
+                    positions_denom.append(int(parts[3]))  # 10n + 3
+                    positions_xyz.append([int(parts[k + 4]) for k in range(3)])  # 10n + 4,5,6
+                    positions_mag.append([int(parts[k + 7]) for k in range(3)])  # 10n + 7,8,9
+
+                og_wyckoff_sites.append((positions_num, positions_denom, positions_xyz, positions_mag))
