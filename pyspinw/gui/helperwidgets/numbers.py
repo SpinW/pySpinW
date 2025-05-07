7fe612376e1642abfdcfbcf5e1af3d5beb40ef76
lucas-wilkins <lucas.wilkins.email@gmail.com>
2025-05-07 11:52:42 +0100

    Unit Cell GUI

--- pyspinw/gui/helperwidgets/numbers.py
+++ pyspinw/gui/helperwidgets/numbers.py
@@ -2,23 +2,75 @@ from PySide6.QtGui import QDoubleValidator
 from PySide6.QtWidgets import QLineEdit, QWidget, QHBoxLayout, QApplication
 from PySide6.QtCore import Signal
 
+import sys
+
 class FloatField(QWidget):
 
-    updated = Signal(float)
 
-    def __init__(self, value: float):
+    changed = Signal(float)
+
+    def __init__(self,
+                 value: float,
+                 bottom: float | None = None,
+                 top: float | None = None,
+                 show_slider: bool = True):
+        """Floating point field wrapper
+
+        :param value: initial value
+        :param bottom: lower bound
+        :param top: upper bound
+        :param show_slider: turn on the slider
+        """
         super().__init__()
 
         self.text_field = QLineEdit(str(value))
 
-        validator = QDoubleValidator(0.0, 100.0, 2, self)
+        self.bottom = bottom
+        self.top = top
+
+        if bottom is None:
+            bottom = -sys.float_info.max
+
+        if top is None:
+            top = sys.float_info.max
+
+        validator = QDoubleValidator(self, bottom=bottom, top=top, decimals=100)
         validator.setNotation(QDoubleValidator.StandardNotation)
         self.text_field.setValidator(validator)
 
         layout = QHBoxLayout()
         layout.addWidget(self.text_field)
 
+        self.text_field.textChanged.connect(self._on_changed)
+
         self.setLayout(layout)
+
+        # Remove padding
+        self.setContentsMargins(0,0,0,0)
+
+
+        self._value = value
+
+    def _on_changed(self):
+        """Event when text is changed"""
+
+        try:
+            potential_value = float(self.text_field.text())
+
+            if self.bottom is None or potential_value >= self.bottom and \
+                    self.top is None or potential_value <= self.top:
+
+                self._value = potential_value
+
+        except ValueError:
+            pass
+
+        self.changed.emit(self.value)
+
+    @property
+    def value(self) -> float:
+        """Floating point value of field"""
+        return self._value
 
 if __name__ == "__main__":
 
--- pyspinw/gui/lattice.py
+++ pyspinw/gui/lattice.py
@@ -1,7 +1,53 @@
-from vispy.app.qt import QWidget
+from PySide6.QtWidgets import QBoxLayout, QGridLayout, QWidget, QApplication, QLabel
+
+from pyspinw.gui.helperwidgets.numbers import FloatField
+from pyspinw.unitcell import UnitCell
+
 
 class LatticeParameters(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, unit_cell: UnitCell | None = None, parent=None):
         super().__init__(parent)
 
-        self.a =
+        self.a = FloatField(1.0, bottom=0.0)
+        self.b = FloatField(1.0, bottom=0.0)
+        self.c = FloatField(1.0, bottom=0.0)
+
+        self.alpha = FloatField(90, 0, 180)
+        self.beta = FloatField(90, 0, 180)
+        self.gamma = FloatField(90, 0, 180)
+
+        layout = QGridLayout(parent=self)
+        # layout.setVerticalSpacing(0)
+        self.setLayout(layout)
+
+        layout.addWidget(QLabel("a"), 0, 0)
+        layout.addWidget(QLabel("b"), 1, 0)
+        layout.addWidget(QLabel("c"), 2, 0)
+
+        layout.addWidget(QLabel("α"), 3, 0)
+        layout.addWidget(QLabel("β"), 4, 0)
+        layout.addWidget(QLabel("γ"), 5, 0)
+
+        layout.addWidget(self.a, 0, 1)
+        layout.addWidget(self.b, 1, 1)
+        layout.addWidget(self.c, 2, 1)
+
+        layout.addWidget(self.alpha, 3, 1)
+        layout.addWidget(self.beta, 4, 1)
+        layout.addWidget(self.gamma, 5, 1)
+
+        layout.addWidget(QLabel("Å"), 0, 2)
+        layout.addWidget(QLabel("Å"), 1, 2)
+        layout.addWidget(QLabel("Å"), 2, 2)
+        layout.addWidget(QLabel("°"), 3, 2)
+        layout.addWidget(QLabel("°"), 4, 2)
+        layout.addWidget(QLabel("°"), 5, 2)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    parameter_widget = LatticeParameters()
+    parameter_widget.show()
+
+    app.exec_()
