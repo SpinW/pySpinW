aaf99711164ed967d9fe822e14a956d9ecbb100f
lucas-wilkins <lucas.wilkins.email@gmail.com>
2025-07-03 18:18:47 +0100

    tolerances lint

--- pyspinw/tolerances.py
+++ pyspinw/tolerances.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Tolerances:
+    """Holds different tolerance values for numerical methods """
+
     SAME_SITE_ABS_TOL = 1e-10
     VECTOR_TOL = 1e-10
     COUPLING_ORDER_THRESHOLD = 1e-7
