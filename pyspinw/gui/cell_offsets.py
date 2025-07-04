dabd1ac80006be8d3fd68de4b2f1d097df76f98e
lucas-wilkins <lucas.wilkins.email@gmail.com>
2025-07-04 15:32:03 +0100

    lint cell_offsets

--- pyspinw/gui/cell_offsets.py
+++ pyspinw/gui/cell_offsets.py
@@ -1,3 +1,5 @@
+""" Cell offsets (i.e. reference to a particular unit cell in a crystal)"""
+
 import numpy as np
 from pydantic import BaseModel
 
@@ -12,17 +14,21 @@ class CellOffset(BaseModel):
     _vector: np.ndarray | None = None
 
     def model_post_init(self, __context):
+        """pydantic: after init"""
         self._vector = np.array([self.i, self.j, self.k], dtype=int)
 
     @property
     def as_tuple(self):
+        """ As a tuple of ints"""
         return self.i, self.j, self.k
 
     @property
     def vector(self):
+        """ Vector of index (int array)"""
         return self._vector
 
     def position_in_supercell(self, supercell_size: tuple[int, int, int]):
+        """ Get the position within a supercell, rather than an infinite crystal (do mods)"""
         si, sj, sk = supercell_size
 
         return CellOffset(
