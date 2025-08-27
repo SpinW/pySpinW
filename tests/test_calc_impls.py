4a1a027c41ad90b91b5f0bcf64792ff9a37d40c0
alexhroom <alex.room@btinternet.com>
2025-08-27 09:22:03 +0100

    added unit tests for comparing spinwave implementations

---
+++ tests/test_calc_impls.py
@@ -0,0 +1,36 @@
+"""Compare spinwave calculation implementations.
+
+This file checks that the the Python and Rust implementations of
+the spinwave calculation both return the same results for a set
+of examples.
+
+"""
+
+import numpy as np
+import pytest
+
+try:
+    from pyspinw.rust import spinwave_calculation as rs_spinwave
+except ImportError:
+    pytestmark = pytest.mark.skip("Rust module not installed.")
+
+from pyspinw.calculations.spinwave import spinwave_calculation as py_spinwave
+
+from examples.raw_calculations.ferromagnetic_chain import heisenberg_ferromagnet
+from examples.raw_calculations.antiferro_chain import antiferro_chain
+from examples.raw_calculations.kagome import kagome_ferromagnet
+from examples.raw_calculations.kagome_antiferro import kagome_antiferromagnet
+from examples.raw_calculations.kagome_supercell import kagome_supercell
+
+@pytest.mark.parametrize("example",
+                         [heisenberg_ferromagnet,
+                          antiferro_chain,
+                          kagome_ferromagnet,
+                          kagome_antiferromagnet,
+                          kagome_supercell,])
+def test_calc_impls(example):
+    """Compare Rust and Python spinwave calculation implementations."""
+    rs_results = rs_spinwave(*example(rust=True))
+    py_results = py_spinwave(*example(rust=False))
+
+    np.testing.assert_allclose(np.sort(rs_results), np.sort(py_results))
