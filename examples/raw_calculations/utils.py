15f70986865664c780b0dd894e83d787baa18625
alexhroom <alex.room@btinternet.com>
2025-09-08 10:07:44 +0100

    added example run utility script

---
+++ examples/raw_calculations/utils.py
@@ -0,0 +1,63 @@
+"""Utility functions for the raw calculation examples."""
+
+from typing import Callable
+
+from pyspinw.calculations.spinwave import spinwave_calculation as py_spinwave, Coupling as PyCoupling
+
+
+def run_example(
+    example: Callable[[int, object], tuple[list, "np.array", "np.array", list]], use_rust=False
+) -> (tuple, "np.array"):
+    """Run an example.
+
+    Parameters
+    ----------
+    example: Callable
+        A function which takes two arguments:
+
+        - n_q: the number of q-values to calculate for
+        - coupling_class: the class to use for coupling objects
+
+        and returns four objects:
+
+        - a list of rotations;
+        - an array of magnitudes;
+        - an array of q-vectors;
+        - a list of couplings.
+
+    use_rust: bool
+        Whether to use the Rust calculation routine.
+
+    Returns
+    -------
+    structure: tuple
+        The structure from the example callable.
+    energies: np.array
+        The result from the spinwave calculation.
+
+    """
+    try:
+        from pyspinw.rust import spinwave_calculation as rs_spinwave, Coupling as RsCoupling
+
+        RUST_AVAILABLE = True
+    except ModuleNotFoundError:
+        RUST_AVAILABLE = False
+
+    # default to Python unless Rust is requested (which it is by default) and available
+    coupling_class = PyCoupling
+    spinwave_calculation = py_spinwave
+    if use_rust:
+        if RUST_AVAILABLE:
+            coupling_class = RsCoupling
+            spinwave_calculation = rs_spinwave
+        else:
+            print(
+                "Attempted to run example in Rust, but Rust is not available. "
+                "To run in Python and hide this warning, "
+                "pass 'python' as an argument to the Python script in the terminal."
+            )
+
+    structure = example(coupling_class=coupling_class)
+    energies = spinwave_calculation(*structure)
+
+    return structure, energies
