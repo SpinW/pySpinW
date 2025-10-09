5aa392b3610b215092d2533cf8eb6eee506beae6
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2025-10-09 15:11:30 +0100

    Decomposition of site/coupling systems into unconnected groups

--- pyspinw/subsystems.py
+++ pyspinw/subsystems.py
@@ -1,25 +1,69 @@
 """ Identification of subsystems of a collection of sites and couplings """
-from collections import defaultdict
 
-import numpy as np
-
-from coupling import Coupling
-from site import LatticeSite, ImpliedLatticeSite
+from pyspinw.coupling import Coupling
+from pyspinw.site import LatticeSite, ImpliedLatticeSite
 
 
-def find_components(sites: list[LatticeSite], couplings: list[Coupling]):
+class Node:
+    """ Graph model for depth first search """
+    def __init__(self):
+        self.connected: list["Node"] = []
+        self.group = -1
+
+    def propagate(self, group) -> bool:
+        """ Search the graph, only follow if not already visited (self.group>=0)"""
+        if self.group == -1:
+            self.group = group
+            for node in self.connected:
+                node.propagate(group)
+
+            return True
+        else:
+            return False
+
+def find_components(sites: list[LatticeSite], couplings: list[Coupling]) \
+        -> list[tuple[list[LatticeSite], list[Coupling]]]:
     """
     Find components (maximal disjoint subsystems) of the given system, or
     in other words, group the sites by being coupled to each other
 
-    Important: Assumes that all the sites given are not parented to sites not in the list"""
+    Important: Assumes that all the sites in the couplings are in the list of sites"""
 
     unique_sites = [site for site in sites if not isinstance(site, ImpliedLatticeSite)]
     n_unique_sites = len(unique_sites)
     unique_id_to_index = {unique_sites[index]._unique_id: index for index in range(n_unique_sites)}
 
-    unique_id_to_sites = defaultdict(list[LatticeSite])
-    for site in sites:
-        unique_id_to_sites[site.parent_site._unique_id].append(site)
+    # Set up graph
+    nodes = [Node() for _ in unique_sites]
 
-    
+    for coupling in couplings:
+        a = unique_id_to_index[coupling.site_1.parent_site._unique_id]
+        b = unique_id_to_index[coupling.site_2.parent_site._unique_id]
+
+        nodes[a].connected.append(nodes[b])
+        nodes[b].connected.append(nodes[a])
+
+    # Depth first graph search
+    group = 0
+    for node in nodes:
+        if node.propagate(group):
+            group += 1
+
+    # collect together into groups
+    site_groups = [[] for _ in range(group)]
+    coupling_groups = [[] for _ in range(group)]
+
+    # Collect sites together
+    for site in sites:
+        node_index = unique_id_to_index[site.parent_site._unique_id]
+        group_index = nodes[node_index].group
+        site_groups[group_index].append(site)
+
+    # Collect couplings together
+    for coupling in couplings:
+        node_index = unique_id_to_index[coupling.site_1.parent_site._unique_id] # Group should be the same for both sites
+        group_index = nodes[node_index].group
+        coupling_groups[group_index].append(coupling)
+
+    # groups as list of tuples
+    return [pair for pair in zip(site_groups, coupling_groups)]
---
+++ tests/test_subsystems.py
@@ -0,0 +1,94 @@
+""" Tests for the decomposition into subsystems"""
+
+from pyspinw.site import LatticeSite, ImpliedLatticeSite
+from pyspinw.coupling import HeisenbergCoupling
+
+from pyspinw.subsystems import find_components
+
+
+def test_no_couplings_no_parenting():
+    """ Check completely unconnected case returns singletons """
+    sites = [LatticeSite(0,0,0),
+             LatticeSite(0,0,0),
+             LatticeSite(0,0,0)]
+
+    components = find_components(sites, [])
+
+    assert len(components) == len(sites), "Should have same number of components as sites"
+    assert all([len(component_sites) == 1 for component_sites, _ in components]), "Components should have one site"
+
+def test_no_couplings_some_parenting():
+    """ Check that parented sites get put in same group"""
+
+    # Use the i component to identify two groups
+    sites = [LatticeSite(0,0,0),
+             LatticeSite(1,0,0)]
+
+    sites += [ImpliedLatticeSite(parent, parent.i, 0, 0) for parent in sites]
+
+    components = find_components(sites, [])
+
+    assert len(components) == 2, "Should have two components"
+    assert all([len(component_sites) == 2 for component_sites, _ in components]), "Each component should have two sites"
+
+    # Check the sites are the right sites
+    for component_sites, _ in components:
+        i = component_sites[0].i
+        for site in component_sites:
+            assert site.i == i, "Sites within component should be linked by parentage"
+
+
+def test_two_systems():
+    """ Two subsystems defined by couplings with no parenting going on"""
+    # expected group encoded in j
+    # site within group encoded in i
+    sites = [LatticeSite(0,0,0),
+             LatticeSite(1,0,0),
+             LatticeSite(0,1,0),
+             LatticeSite(1,1,0)]
+
+    couplings = [
+        HeisenbergCoupling(sites[0], sites[1], 1),
+        HeisenbergCoupling(sites[2], sites[3], 1)]
+
+    components = find_components(sites, couplings)
+
+    assert len(components) == 2, "There should be two components"
+
+    for component_sites, component_couplings in components:
+        expected_group_index = component_sites[0].j # expected group encoded in j
+
+        assert len(component_sites) == 2, "Each group should contain two sites"
+        assert len(component_couplings) == 1, "Each group should contain one coupling"
+
+        for site in component_sites:
+            assert site.j == expected_group_index, "Site should be in expected group"
+
+        for coupling in component_couplings:
+            assert coupling.site_1.j == expected_group_index, "Coupling should refer to site in expected group"
+            assert coupling.site_2.j == expected_group_index, "Coupling should refer to site in expected group"
+
+
+def test_parenting_connected_two_systems():
+    """ Same as the two system test, except the non-coupled sites are connected by parentage,
+    therefore there should only be a single group"""
+
+    sites = [LatticeSite(0, 0, 0),
+             LatticeSite(1, 0, 0)]
+
+    sites += [ImpliedLatticeSite(parent, parent.i, 0, 0) for parent in sites]
+
+    couplings = [
+        HeisenbergCoupling(sites[0], sites[1], 1),
+        HeisenbergCoupling(sites[2], sites[3], 1)]
+
+    components = find_components(sites, couplings)
+
+    assert len(components) == 1, "There should only be one subsystem"
+
+    component_sites, component_couplings = components[0]
+
+    assert len(component_sites) == 4, "The single component should have 4 sites"
+    assert len(component_couplings) == 2, "The single component should have 2 couplings"
+
+
