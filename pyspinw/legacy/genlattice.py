068738e5d660df80175f927b82b04e1c002b7b5f
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2025-10-30 16:54:35 +0000

    Finish genlattice

--- pyspinw/legacy/genlattice.py
+++ pyspinw/legacy/genlattice.py
@@ -1,14 +1,15 @@
 """ This will hold code that implements genlattice for pyspinw"""
+from dataclasses import dataclass
+
 import numpy as np
 from numpy._typing import ArrayLike
 
 from pyspinw.checks import check_sizes
-from pyspinw.site import LatticeSite
-from pyspinw.symmetry.group import SpaceGroup, spacegroup_by_name
+from pyspinw.symmetry.group import SpaceGroup
+from pyspinw.interface import spacegroup as lookup_spacegroup
 from pyspinw.symmetry.unitcell import UnitCell
-from ase.geometry.cell import cell_to_cellpar
 
-
+@dataclass
 class GenLatticeResult:
     """ Result from a genlattice call """
 
@@ -23,9 +24,28 @@ def genlattice(
         angle: ArrayLike | None = None,
         angled: ArrayLike | None = None,
         spgr: SpaceGroup | str = "P1"):
-    """ TODO: Docstring """
-    # a better name
-    spacegroup = spgr
+
+    """ pySpinW version of MATLAB spinW's genlattice
+
+    This works similarly, though the definition of spacegroups is a little different.
+
+    :param lat_const: lattice constants a,b and c
+    :param angle: unit cell angles alpha, beta and gamma in radians (you need to specify either this or angled)
+    :param angled: unit cell angles alpha, beta and gamma in degrees (you need to specify either this or angle)
+    :param spgr: spacegroup, either a search string (see `pyspinw.interface.spacegroup`) or a SpaceGroup object
+
+    :return: GenLatticeResult, an object that holds the information generated, which can be fed to genmagstr
+    """
+
+    if isinstance(spgr, str):
+        spacegroup = lookup_spacegroup(spgr)
+
+    elif isinstance(spgr, SpaceGroup):
+        spacegroup = spgr
+
+    else:
+        raise TypeError("Expected `spgr` type to be either `str` or `SpaceGroup`")
+
 
     if angle is None and angled is None:
         raise ValueError("Expected to have either 'angle' or 'angled' specified.")
@@ -36,11 +56,6 @@ def genlattice(
     if angled is None:
         angled = (180/np.pi)*angle
 
-    # Resolve the spacegroup if specified by name
-
-    if isinstance(spacegroup, str):
-        spacegroup = spacegroup_by_name(spacegroup)
-
     # Check compatability with cell definition
 
     base_cell = UnitCell(a=lat_const[0],
@@ -50,7 +65,11 @@ def genlattice(
                          beta=angled[1],
                          gamma=angled[2])
 
+    # Validate the cell
+    spacegroup.lattice_system.validate(base_cell)
+
     return GenLatticeResult(spacegroup, base_cell)
 
 if __name__ == "__main__":
-    genlattice(angle=(1,2,3), lat_const=(1,2,3), spgr="p7")
+    res = genlattice(angled=(40,50,60), lat_const=(1,2,3), spgr="p1")
+    print(res)
