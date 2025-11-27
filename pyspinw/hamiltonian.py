49b4ff5c81e5e1291be6e6bbf11d3581bf77fd44
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2025-11-27 20:33:23 +0000

    Path class

--- pyspinw/hamiltonian.py
+++ pyspinw/hamiltonian.py
@@ -2,9 +2,11 @@
 
 from abc import ABC, abstractmethod
 
-from pyspinw._base import MagneticStructure
+from pyspinw.coupling import Coupling
 from pyspinw.serialisation import SPWSerialisable
 import numpy as np
+
+from pyspinw.structures import Structure
 
 
 # pylint: disable=R0903
@@ -15,41 +17,19 @@ class Hamiltonian(ABC, SPWSerialisable):
     serialisation_name = "hamiltonian"
 
     def __init__(self,
-                 magnetic_structure: MagneticStructure,
+                 structure: Structure,
                  couplings: list[Coupling]):
 
-        self.magnetic_structure = magnetic_structure
+        self.structure = structure
         self.couplings = couplings
-
-    def get_structure(self):
-        """Get the structural properties of the Hamiltonian needed for calculation."""
-        raise NotImplementedError
-        # sites = self.magnetic_structure.get_sites()
-        # symmetry_stuff = self.magnetic_structure.get_symmetry()
-        # etc....
-        # return (sites, symmetry_stuff, etc...)
 
     @abstractmethod
     def energies(self, q_vectors: np.ndarray):
         """Calculate the energy levels of the system for the given q-vectors."""
-        # this will have a specific implementation for each Hamiltonian
-        raise NotImplementedError
+
+
 
     def _serialise(self) -> dict:
-        return {"magnetic_structure": self.magnetic_structure._serialise(),
+        return {"magnetic_structure": self.structure._serialise(),
                 "couplings": [coupling._serialise() for coupling in self.couplings]}
 
-
-class GeneralHamiltonian(Hamiltonian):
-    """Hamiltonian of the form: H = Σ s.P.s + Q.s
-
-    More explicitly, the equation is ... TODO
-
-    """
-
-
-class BiQuadratic(Hamiltonian):
-    """Biquadratic Hamiltonian of the form H = - Σ A (s.s)^2
-
-    More explicitly, the equation is ... TODO
-    """
