04a04fee5db54e1b2a65155535642922210362e4
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2026-01-13 09:20:47 +0000

    Powder stuff done

--- examples/structures/powder_spectrum.py
+++ examples/structures/powder_spectrum.py
@@ -39,4 +39,4 @@ if __name__ == "__main__":
     sample = Powder(hamiltonian)
 
     path1D = Path1D(0.1, 0.9, resolution=201)
-    sample.show_spectrum(path1D)
+    sample.show_spectrum(path1D, n_energy_bins=20)
