5f2081e4b2e9bfd2894390af41bb5a914921c998
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2026-03-28 00:20:30 +0000

    Test for consistency

---
+++ tests/test_packaging.py
@@ -0,0 +1,15 @@
+import types
+
+def test__all__is_present():
+    """ Check that the names in __all__ in the __init__ are available the imports """
+    import pyspinw
+
+    exported = set(pyspinw.__all__)
+
+    actual = {
+        name for name in dir(pyspinw)
+        if not name.startswith("_") # Ignore private
+           and not isinstance(getattr(pyspinw, name), types.ModuleType) # Ignore modules
+    }
+
+    assert exported == actual, "__all__ should match imports in __init__"
