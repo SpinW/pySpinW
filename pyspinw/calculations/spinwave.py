21de8af3ce15f45ee4eaf367fcc688460d86b5e7
Duc Le <duc.le@stfc.ac.uk>
2026-04-10 18:27:07 +0100

    Remove orthogonalisation code in spinwave.py

--- pyspinw/calculations/spinwave.py
+++ pyspinw/calculations/spinwave.py
@@ -16,8 +16,6 @@ from pyspinw.windows_parallelisation import windows_python_parallelisation_enabl
 ZERO_ENERGY_TOL = 1e-12
 # tolerance of diagonal matrix elements to be considered singular
 SINGULAR_TOL = 1e-7
-# tolerance to determine of energy levels are degenerate
-DEGEN_TOL = 1e-5
 
 # Disable linting for bad variable names, because they should match the docs
 # ruff: noqa: E741
@@ -210,13 +208,6 @@ def _solve_ham_hermitian(hamiltonian_matrix: np.ndarray, n_sites: int):
 def _solve_ham_nonherm(hamiltonian_matrix: np.ndarray, n_sites:int):
     hamiltonian_matrix[n_sites:, :] *= -1  # gComm * ham
     eigvals, eigvecs = np.linalg.eig(hamiltonian_matrix)
-    # We need to orthogonalise eigenvectors of degenerate (real part of) eigenvalues
-    isr = np.argsort(eigvals)
-    eigvals, eigvecs, real_en = (eigvals[isr], eigvecs[:,isr], np.real(eigvals[isr]))
-    for en in real_en[np.append([1.], np.diff(real_en)) > DEGEN_TOL]:
-        idx = np.where(np.abs(real_en - en) < DEGEN_TOL)[0]
-        U, _, _ = np.linalg.svd(eigvecs[:,idx], full_matrices=False)
-        eigvecs[:,idx] = U
     gV = eigvecs.copy()
     gV[:n_sites, n_sites:] *= -1
     gV[n_sites:, :n_sites] *= -1
