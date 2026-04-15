c5cd8e7de567b36966031892d44bea8db569c380
Duc Le <duc.le@stfc.ac.uk>
2026-04-15 05:40:43 +0100

    Add examples 4 (polarised)

---
+++ examples/problems/04_polarised.py
@@ -0,0 +1,23 @@
+"""
+In this worksheet we will explore doing a simulation of an XYZ polarised experiment
+"""
+
+from pyspinw import *
+
+# Set up a J1-J2 square antiferromagnet structrure with nearest and next-nearest
+# antiferromagnetic Heisenberg exchange interaction, with J2 less than 0.5J1
+# and a propagation vector of (0.5, 0.5, 0) and moments in the a-b plane
+
+???
+
+# Now plot the (unpolarised) dispersion along the (100) and (110) directions
+
+???
+
+# Now plot the same dispersion with Pxx, Pyy, Pzz polarisation,
+# assuming that the sample was mounted with the a-b plane horizontal
+# These are the components we can measure with longitudinal polarisation analysis
+
+???
+
+# QUESTION: What do you notice about the spectra compared to the unpolarised?
