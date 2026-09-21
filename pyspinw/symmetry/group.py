734d558f4618f2e2e741f31be70b0b444a362f06
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2026-09-21 23:49:47 +0100

    Filled in example in spacegroups_with_operations docstring

--- pyspinw/symmetry/group.py
+++ pyspinw/symmetry/group.py
@@ -466,7 +466,7 @@ class SpacegroupDatabase:
     def spacegroups_with_operations(self, operation_string: str) -> ExactMatch | PartialMatch:
         """ Get a spacegroup by a list of operations
 
-        Example: TODO
+        Example: "x, y, z; -x, -y, -z" should give P1
 
         :param operation_string: A list of operations in the x,y,z form, semicolon separated
         :return: ExactMatch or MultipleMatches
